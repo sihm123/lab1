ea2350bbcb405d09a0e9b5e3720cb605655dfd4e
sihm123 <sihm@clemson.edu>
2024-09-08 22:14:01 -0400

    Update retirementCalc.cpp

--- retirementCalc.cpp
+++ retirementCalc.cpp
@@ -12,10 +12,10 @@
 using namespace std;
 
 int main(){
-
+        //variables are declared
         int t;
         double p,r,rSavings;
-
+        //asks the user to input principle, interest rate, time
         cout<<"enter principal: ";
         cin>>p;
 
@@ -24,8 +24,9 @@ int main(){
 
         cout<<"enter number of years: ";
         cin>>t;
-
+        //calls the function given user inputs to calculate retirement savings
         rSavings = compoundCalc(p,r,t);
+        //prints out retirement savings
         cout<<"Your retirement savings will be $"<<rSavings<<endl;
 
         return(0);
