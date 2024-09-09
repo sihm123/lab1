9e585bd93ce4badb851b93b4c817afddcf3f8aca
sihm123 <sihm@clemson.edu>
2024-09-08 21:45:53 -0400

    Create compoundCalc.h

---
+++ compoundCalc.h
@@ -0,0 +1,14 @@
+//Samantha Ihm
+//08 Sept 2024
+//CPSC 1021 Lab 1
+//compoundCalc.h
+//this program is the header file for the compoundCalc function
+//5 min
+
+
+#ifndef COMPOUNDCALC_H
+#define COMPOUNDCALC_H
+
+double compoundCalc(double, double, int);
+
+#endif
